6c04cfe4ce1b8a26328f3a0bd27436261b526234
Yang <didtdjwns111@gmail.com>
2023-01-05 23:18:28 +0900

    연속된 수의 합

---
+++ src/programmers/Lv0/연속된_수의_합.java
@@ -0,0 +1,35 @@
+package programmers.Lv0;
+
+import java.util.Arrays;
+
+public class 연속된_수의_합 {
+    public static void main(String[] args) {
+
+        int num = 3;
+        int total = 12;
+        System.out.println(Arrays.toString(solution(num, total)));	//[3, 4, 5]
+
+        num = 5;
+        total = 15;
+        System.out.println(Arrays.toString(solution(num, total)));	//[1, 2, 3, 4, 5]
+
+        num = 4;
+        total = 14;
+        System.out.println(Arrays.toString(solution(num, total)));	//[2, 3, 4, 5]
+
+        num = 5;
+        total = 5;
+        System.out.println(Arrays.toString(solution(num, total)));	//[-1, 0, 1, 2, 3]
+    }
+
+    public static int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int idx = total % num == 0 ? num / 2 : num / 2 - 1 ;
+
+
+        for (int i = 0; i < num; i++) {
+            answer[i] = total / num - idx + i;
+        }
+        return answer;
+    }
+}
