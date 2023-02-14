76355a321839a8515ae36f4fc3efd9e8c3463a5b
jjuuuunn <didtdjwns111@gmail.com>
2023-02-14 19:44:03 +0900

    평균 구하기

---
+++ src/ProgrammersSchool/Java/Lv1/평균_구하기.java
@@ -0,0 +1,18 @@
+package ProgrammersSchool.Java.Lv1;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class 평균_구하기 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(solution(arr)); // 2.5
+
+        arr = new int[] {5, 5};
+        System.out.println(solution(arr)); // 5
+    }
+
+    public static double solution(int[] arr) {
+        return Arrays.stream(arr).average().getAsDouble();
+    }
+}
