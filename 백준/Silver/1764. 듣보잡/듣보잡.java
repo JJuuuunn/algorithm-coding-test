fe3546fbaa3304700d384ccdea70774477fc0999
양성준 <100821691+JJuuuunn@users.noreply.github.com>
2023-04-21 19:33:51 +0900

    [Silver IV] Title: 듣보잡, Time: 284 ms, Memory: 26180 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (set.contains(name)) {
+                cnt++;
+                list.add(name);
+            }
+        }
+        Collections.sort(list);
+
+        System.out.println(cnt);
+
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
