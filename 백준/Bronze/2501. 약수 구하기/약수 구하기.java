85173779b0c8afd6a36e744bbb15c4fca92ef972
양성준 <100821691+JJuuuunn@users.noreply.github.com>
2023-06-06 21:53:14 +0900

    [Bronze III] Title: 약수 구하기, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        int idx = 0;
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                cnt++;
+                if (cnt == K) {
+                    idx = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(idx);
+    }
+}
