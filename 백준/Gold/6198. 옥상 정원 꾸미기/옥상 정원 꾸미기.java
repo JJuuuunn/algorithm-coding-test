23fe3c5ff7ebed643be99f032537087ef49d8013
양성준 <100821691+JJuuuunn@users.noreply.github.com>
2023-06-12 18:40:55 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 284 ms, Memory: 23540 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -7,21 +7,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] apt = new int[N];
         long total = 0l;
 
-        for (int i = 0; i < N; i++) {
-            apt[i] = Integer.parseInt(br.readLine());
-        }
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (apt[i] > apt[j]) {
-                    total++;
-                } else {
-                    break;
-                }
+        for (int i = 0; i < N; i++) {
+            int h = Integer.parseInt(br.readLine());
+
+            while (!stack.isEmpty() && (stack.peek() <= h)) {
+                stack.pop();
             }
+            stack.push(h);
+            total += stack.size() - 1;
         }
 
         System.out.println(total);
