df2e5b6f943fe3d7d683814475306f3fa40c9acd
양성준 <100821691+JJuuuunn@users.noreply.github.com>
2023-06-19 16:52:08 +0900

    [Silver IV] Title: 암기왕, Time: 1844 ms, Memory: 228204 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,30 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int t = Integer.parseInt(br.readLine());
-
-        for (int j = 0; j < t; j++) {
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
-            int[] arr1 = new int[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                arr1[i] = Integer.parseInt(st.nextToken());
+            int[] NArr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                NArr[j] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(arr1);
+            Arrays.sort(NArr);
 
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                int num = Integer.parseInt(st.nextToken());
-                if (Arrays.binarySearch(arr1, num) >= 0) {
-                    sb.append("1").append("\n");
-                } else sb.append("0").append("\n");
+
+            for (int j = 0; j < M; j++) {
+                if (Arrays.binarySearch(NArr, Integer.parseInt(st.nextToken())) >= 0) sb.append("1");
+                else sb.append("0");
+                sb.append("\n");
             }
         }
         System.out.println(sb);
