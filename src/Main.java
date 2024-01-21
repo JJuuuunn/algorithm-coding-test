26ead89c3ea4ab6494717f667371b7b32b47f652
JJuuuunn <didtdjwns111@gmail.com>
2024-01-21 11:11:11 +0900

    으아아아아 공부하기 귀찮다. 안되는걸 알면서 이러는 중

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+class Main {
+
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
     }
 }
