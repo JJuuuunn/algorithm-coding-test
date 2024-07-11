7001f73cd5370352fb1fe235d65ad758e928c418
Yang Seong Jun <100821691+JJuuuunn@users.noreply.github.com>
2024-07-11 12:47:15 +0900

    [Bronze III] Title: 웰컴 키트, Time: 124 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+        
+		int[] a = new int[6];
+		
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < 6; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+        
+        st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		
+		int count = 0;
+		for(int i = 0; i < 6; i++) {
+			count += a[i] / t;
+			if(a[i] % t != 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		System.out.println(n / p + " " + n % p);
+	}
+}
