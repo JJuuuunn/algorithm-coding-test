bc51910dbd171c03511fb605df9b74843c8231c1
Yang Seong Jun <100821691+JJuuuunn@users.noreply.github.com>
2024-07-17 22:32:40 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 8.81 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,16 +2,12 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
-        // 첫 글자인지 체크
         boolean check = true;
         
-        // s를 전부 소문자로 변경 후 끊어서 sArr배열에 주입
         String[] sArr = s.toLowerCase().split("");
         
         for (String str : sArr) {
-            // 첫 글자이면 대문자로 변경 아니면 그대로 주입
             answer += (check ? str.toUpperCase() : str);
-            // 첫 글자 체크를 위한 조건문
             check = (str.equals(" ") ? true : false);
         }
 
