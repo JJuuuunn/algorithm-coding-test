101e7ac330e5731c1e24db5bef1124bcdf4b4598
Yang Seong Jun <100821691+JJuuuunn@users.noreply.github.com>
2025-02-20 14:09:22 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,4 @@
 function solution(my_string, overwrite_string, s) {
-    let str = my_string.split('');
-    str.splice(s, overwrite_string.length, overwrite_string);
-    return str.join("");
+        return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
+
 }
